e42bbc59b95a294246543fdce8ec87849a176e8c
ikergf <79423341+ikergf@users.noreply.github.com>
2024-02-25 23:26:50 +0100

    Update utils.py

--- week1/utils.py
+++ week1/utils.py
@@ -9,14 +9,6 @@ import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 
-# from keras.optimizers import SGD, Adam, Adadelta, RMSprop
-# from tensorflow.keras.preprocessing.image import ImageDataGenerator
-# from keras.layers import Dense, Dropout, BatchNormalization, Conv2D, MaxPooling2D, Activation, AveragePooling2D, Input
-# from keras.regularizers import l2
-# from keras.models import Model, Sequential
-# from keras.layers import Dense, GlobalAveragePooling2D
-# import matplotlib.pyplot as plt
-
 
 class MiTDataset(Dataset):
     def __init__(self, data_dir, transform=None):
@@ -138,24 +130,11 @@ def data_augmentation(augment: bool, params, width, height):
         mean=[0.43239, 0.45149, 0.44213],
         std=[0.255, 0.244, 0.270]
       )])
-    # else:
-    #   data_generator = ImageDataGenerator(
-    #     #rescale=1./255,
-    #     preprocessing_function = lambda x: custom_preprocess(x, mean_values= mean_values, std=std),
-    #     rotation_range=rotation_range,
-    #     width_shift_range=width_shift_range,
-    #     height_shift_range=height_shift_range,
-    #     shear_range=shear_range,
-    #     zoom_range=zoom_range,
-    #     horizontal_flip=horizontal_flip,
-    #     fill_mode = 'nearest'
-    #   )
         
     return transform
 
   else:
-    # if mean_values[0] == 0:
-    #   return ImageDataGenerator(rescale=1./255)
+
     transform = transforms.Compose([
                 transforms.Resize((width, height)),
                 transforms.ToTensor(),
@@ -182,60 +161,6 @@ def get_optimizer(params, model):
         optimizer = torch.optim.RMSprop(model.parameters(), lr=float(params['lr']))
 
     return optimizer
-
-
-# def softmax(x):
-#     """Compute softmax values for each sets of scores in x."""
-#     e_x = np.exp(x - np.max(x))
-#     return e_x / e_x.sum(axis=0)
-
-
-# def generate_image_patches_db(in_directory,out_directory,patch_size=64):
-#   if not os.path.exists(out_directory):
-#       os.makedirs(out_directory)
- 
-#   total = 2688
-#   count = 0  
-#   for split_dir in os.listdir(in_directory):
-#     if not os.path.exists(os.path.join(out_directory,split_dir)):
-#       os.makedirs(os.path.join(out_directory,split_dir))
-  
-#     for class_dir in os.listdir(os.path.join(in_directory,split_dir)):
-#       if not os.path.exists(os.path.join(out_directory,split_dir,class_dir)):
-#         os.makedirs(os.path.join(out_directory,split_dir,class_dir))
-  
-#       for imname in os.listdir(os.path.join(in_directory,split_dir,class_dir)):
-#         count += 1
-#         im = Image.open(os.path.join(in_directory,split_dir,class_dir,imname))
-#         print(im.size)
-#         print('Processed images: '+str(count)+' / '+str(total), end='\r')
-#         patches = image.extract_patches_2d(np.array(im), (64, 64), max_patches=1)
-#         for i,patch in enumerate(patches):
-#           patch = Image.fromarray(patch)
-#           patch.save(os.path.join(out_directory,split_dir,class_dir,imname.split(',')[0]+'_'+str(i)+'.jpg'))
-#   print('\n')
-
-# def conv_block(n_filters, kernel_size, stride, bn, dp, pool, padding, input=0):
-  
-#   #if input !=0:
-#   x = Conv2D(n_filters, (kernel_size,kernel_size), strides = (stride, stride), padding=padding)(input)
-#   #else:
-#     #model.add(Conv2D(n_filters, (kernel_size,kernel_size), strides = (stride, stride), padding=padding))
-  
-#   if bn == 'True':
-#    x = BatchNormalization()(x)
-  
-#   x = Activation('relu')(x)
-  
-#   if pool == 'max':
-#     x = MaxPooling2D(pool_size=2)(x)
-#   elif pool == 'avg':
-#     x = AveragePooling2D(pool_size=2)(x)
-
-#   if dp != 0:
-#     x = Dropout(dp)(x)
-
-#   return x
      
 class ConvBlock(nn.Module):
   def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=True, pooling='max', batch_norm=True, dropout=0):
@@ -304,83 +229,12 @@ class Model(nn.Module):
           x = self.pool(x)
           x = self.conv_block4(x)
 
-    x = self.pool(x)    
+    # x = self.pool(x)    
     x = self.globavgpool(x)
     x = torch.flatten(x, 1)
     x = self.fc(x)
     x = self.relu(x)
     x = self.fc2(x)
-    x = F.softmax(x,dim=1)
+    # x = F.softmax(x,dim=1)
     return x
-
-
-# def build_model(params, input):
-  
-#   #model = Sequential()
-#   # Input layer
-
-#   x = conv_block(int(params['n_filters_1']), int(params['kernel_size_1']), params['stride'], params['bn'], float(params['dropout']), params['pool'], params['padding'], input)
-
-#   if params['depth'] > 1:
-#     x = conv_block(int(params['n_filters_2']), int(params['kernel_size_2']), params['stride'], params['bn'], float(params['dropout']), params['pool'], params['padding'], x)
-
-#     if params['depth'] > 2:
-#       x = conv_block(int(params['n_filters_3']), int(params['kernel_size_3']), params['stride'], params['bn'], float(params['dropout']), params['pool'], params['padding'], x)
-    
-#       if params['depth'] > 3: 
-#         x = conv_block(int(params['n_filters_4']), int(params['kernel_size_4']), params['stride'], params['bn'], float(params['dropout']), params['pool'], params['padding'], x)
-
-
-#   model = Model(inputs = input, outputs = x)
-#   if params['pool']!='none':
-#     # Create a new Sequential model
-#     model = Model(inputs = input, outputs = model.layers[-1].output)
-#     x = model.output
-
-#     # Remove the last layer
-#     #model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
-
-#   #model.add(MaxPooling2D(pool_size=2))
-#   x= GlobalAveragePooling2D()(x)
-
-#   x=Dense(int(params['neurons']))(x)
-#   #if params['bn'] == 'True':
-#   #  model.add(BatchNormalization())
-#   x = Activation('relu')(x)
-
-#   x = Dense(params['output'], activation='softmax')(x)
-#   model = Model(inputs = input, outputs = x)
-#   model.summary()
-
-#   return model
-
-
-# def prune_model(model, pruning_threshold=0.1):
-#   for layer in model.layers[:-1]:
-#     if isinstance(layer, Dense):
-#       weights = layer.get_weights()
-#       pruned_weights = [w * (np.abs(w) >= pruning_threshold) for w in weights]
-#       layer.set_weights(pruned_weights)
-
-#   # Count the number of parameters in the pruned model
-#   num_parameters_pruned = model.count_params()
-#   print('Num of params after pruning: ', num_parameters_pruned)
-
-#   return 
-
-# def visualize_weights(model):
-#   dense = 0
-#   for layer in model.layers[:-1]:
-#     if isinstance(layer, Dense):
-#       weights = layer.get_weights()[0]
-#       # Flatten the weights to a 1D array
-#       weights_flat = weights.flatten()
-
-#       # Plot the histogram of weight values
-#       plt.hist(weights_flat, bins=50, color='blue', edgecolor='black')
-#       plt.title('Histogram of Weight Values')
-#       plt.xlabel('Weight Value')
-#       plt.ylabel('Frequency')
-#       plt.savefig(f'weights_{dense}.png')
-#       dense+=1
       
